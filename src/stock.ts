d815dff96556f9d2d3cdd7b0350fda6b881f9a4d
ohko <ohko@qq.com>
2020-09-07 22:13:07 +0800

    optimize code

--- src/stock.ts
+++ src/stock.ts
@@ -3,19 +3,33 @@ import Axios from 'axios';
 
 class TreeItem extends vscode.TreeItem {
    code!: string;
-   yestodayPrice!: number;
-   nowPrice!: number;
-   costPrice!: number;
+   name!: string;
+   yestodayPrice: number;
+   nowPrice: number;
+   costPrice: number;
+   per!: number;
+   time: string;
+   tips: string
 
-   constructor(label: string, collapsibleState?: vscode.TreeItemCollapsibleState) {
-      super(label, collapsibleState)
+   constructor(code: string, name: string, yestodayPrice: number, nowPrice: number, costPrice: number, time: string, tips: string, collapsibleState?: vscode.TreeItemCollapsibleState) {
+      super(name, collapsibleState)
+
+      if (name.length != 4) name += "    ".repeat(4 - name.length)
+
+      this.code = code
+      this.name = name
+      this.yestodayPrice = yestodayPrice
+      this.nowPrice = nowPrice
+      this.costPrice = costPrice
+      this.time = time
+      this.tips = tips
    }
 }
 
 export class StockListProvider implements vscode.TreeDataProvider<TreeItem> {
    private context: vscode.ExtensionContext;
    private panel: vscode.WebviewPanel | undefined;
-   private stockStatus: string[] = []
+   private stockList: TreeItem[] = []
    private cache: Map<string, string> = new Map<string, string>()
 
    constructor(context: vscode.ExtensionContext) {
@@ -34,28 +48,57 @@ export class StockListProvider implements vscode.TreeDataProvider<TreeItem> {
    refresh(): void { this._onDidChangeTreeData.fire() }
 
    getTreeItem(element: TreeItem): TreeItem {
+      // arrow
+      let arrow = "     "
+      if (element.costPrice > 0) {
+         if (element.nowPrice > element.costPrice) arrow = " ↑ "
+         else if (element.nowPrice < element.costPrice) arrow = " ↓ "
+      } else {
+         if (element.yestodayPrice > element.nowPrice) arrow = " ↓ "
+         else if (element.yestodayPrice < element.nowPrice) arrow = " ↑ "
+      }
+
+      // diffSecond
+      let now = new Date()
+      let diffSecond = (now.getTime() - new Date(now.toLocaleDateString() + " " + element.time).getTime()) / 1000
+
+      let label = (element.tips.length == 0 ? element.name : "") + arrow + "(" + element.per.toFixed(2) + "%) " + "¥" + (element.tips.length == 0 ? element.nowPrice : element.costPrice) + element.tips
+      if (diffSecond > 180) label += " ∞"
+      else if (diffSecond > 60) label += " +" + diffSecond.toFixed(0) + "s"
+      element.label = label
+
       return element
    }
 
    getChildren(element?: TreeItem): vscode.ProviderResult<TreeItem[]> {
       let hasChildren = []
-      for (let i in this.stockStatus) {
-         let name = this.stockStatus[i].split(" ")[0].trim()
-         if (this.stockStatus[i].indexOf("[") > 0) hasChildren.push(name)
+      for (let i in this.stockList) {
+         if (this.stockList[i].tips.length > 0) hasChildren.push(this.stockList[i].name)
       }
 
       let list = []
-      for (let i in this.stockStatus) {
-         let x = this.stockStatus[i]
-         let name = this.stockStatus[i].split(" ")[0].trim()
-         if (!element && x.indexOf("[") > 0) continue
-         if (element && (x.indexOf("[") < 0 || element.label!.split(" ")[0].trim() != name)) continue
-         let tic = vscode.TreeItemCollapsibleState.None
-         if (hasChildren.includes(name) && !element) tic = vscode.TreeItemCollapsibleState.Expanded
+      for (let i in this.stockList) {
+         let x = this.stockList[i]
+         if (!element && x.tips.length > 0) continue
+         if (element && (x.tips.length == 0 || element.label!.split(" ")[0].trim() != x.name)) continue
+         x.collapsibleState = vscode.TreeItemCollapsibleState.None
+         if (hasChildren.includes(x.name) && !element) x.collapsibleState = vscode.TreeItemCollapsibleState.Expanded
 
-         let item = new TreeItem(x, tic)
-         list.push(item)
+         // per
+         x.per = (x.nowPrice - x.yestodayPrice) / x.yestodayPrice * 100
+         if (x.costPrice > 0) {
+            x.per = (x.nowPrice - x.costPrice) / x.costPrice * 100
+         }
+
+         list.push(x)
       }
+
+      list.sort((x, y): number => {
+         if (x.name == "上证指数" || x.name == "深证指数") return 1
+         if (y.name == "上证指数" || y.name == "深证指数") return 1
+         return x.per > y.per ? -1 : 1
+      })
+
       return list
    }
 
@@ -72,11 +115,12 @@ export class StockListProvider implements vscode.TreeDataProvider<TreeItem> {
          if (t.getDay() == 0 || t.getDay() == 6) return
       }
 
-      this.stockStatus.length = 0
+      this.stockList.length = 0
       for (let i in codes) {
          let code = codes[i]
          let base = 0.0
          let tips = ""
+
          if (code.indexOf(",") > 0) {
             let tmp = code.split(",")
             code = tmp[0].trim()
@@ -91,47 +135,25 @@ export class StockListProvider implements vscode.TreeDataProvider<TreeItem> {
             name = JSON.parse('["' + res.data.split("~")[2] + '"]')[0]
             this.cache.set(code, name)
          }
-         if (!this.cache.has(name)) this.cache.set(name, code)
 
          let rs = await Axios.get("http://hq.sinajs.cn/list=" + code)
          let arr = rs.data.split(",")
-         let price = arr[3]
-         let yestoday = arr[2]
-         if (base > 0) yestoday = base
+         let yestoday = parseFloat(arr[2])
+         let price = parseFloat(arr[3])
          let time = arr[31]
-         // if (parseInt(i) > 0) time = ""
-         let now = new Date()
-         let diffSecond = (now.getTime() - new Date(now.toLocaleDateString() + " " + time).getTime()) / 1000
-         let per = (price - yestoday) / yestoday * 100
-         // html.push(time + name + (yestoday > price ? " ↓" : " ↑") + price + " (" + per.toFixed(2) + "%)")
-         if (name.length != 4) name += "    ".repeat(4 - name.length)
-         let arrow = " "
-         if (yestoday > price) arrow = " ↓ "
-         else if (yestoday < price) arrow = " ↑ "
-         let msg = name + arrow + "(" + per.toFixed(2) + "%) " + "¥" + price + tips
-         if (diffSecond > 180) msg += " ∞"
-         else if (diffSecond > 60) msg += " +" + diffSecond.toFixed(0) + "s"
-         this.stockStatus.push(msg)
+
+         this.stockList.push(new TreeItem(code, name, yestoday, price, base, time, tips))
       }
-      this.stockStatus.sort((x, y): number => {
-         if (x.substr(0, 4) == "上证指数" || x.substr(0, 4) == "深证指数") return 1
-         if (y.substr(0, 4) == "上证指数" || y.substr(0, 4) == "深证指数") return 1
-         let _x = x.split("(")
-         let _y = y.split("(")
-         return parseFloat(_x[1]) > parseFloat(_y[1]) ? -1 : 1
-      })
 
       this.refresh()
    }
 
-   async itemClick(x: any) {
-      let name = x.label.split(" ")[0].trim()
-      let code = this.cache.get(name)!
-      try { this.panel!.title = name } catch (e) { this.panel = vscode.window.createWebviewPanel(code, name, vscode.ViewColumn.One) }
-      let html = "<table><tr><td><img src='http://image.sinajs.cn/newchart/min/n/" + this.cache.get(x.label!.split(" ")[0].trim()) + ".gif?_+" + Math.random() + "'></td>"
-      html += "<td><img src='http://image.sinajs.cn/newchart/daily/n/" + this.cache.get(x.label!.split(" ")[0].trim()) + ".gif?_+" + Math.random() + "'></td></tr>"
-      html += "<tr><td><img src='http://image.sinajs.cn/newchart/weekly/n/" + this.cache.get(x.label!.split(" ")[0].trim()) + ".gif?_+" + Math.random() + "'></td>"
-      html += "<td><img src='http://image.sinajs.cn/newchart/monthly/n/" + this.cache.get(x.label!.split(" ")[0].trim()) + ".gif?_+" + Math.random() + "'></td></tr></table>"
+   async itemClick(x: TreeItem) {
+      try { this.panel!.title = x.name } catch (e) { this.panel = vscode.window.createWebviewPanel(x.code, x.name, vscode.ViewColumn.One) }
+      let html = "<script>alert(1)</script><table><tr><td><img src='http://image.sinajs.cn/newchart/min/n/" + x.code + ".gif?_+" + Math.random() + "'></td>"
+      html += "<td><img src='http://image.sinajs.cn/newchart/daily/n/" + x.code + ".gif?_+" + Math.random() + "'></td></tr>"
+      html += "<tr><td><img src='http://image.sinajs.cn/newchart/weekly/n/" + x.code + ".gif?_+" + Math.random() + "'></td>"
+      html += "<td><img src='http://image.sinajs.cn/newchart/monthly/n/" + x.code + ".gif?_+" + Math.random() + "'></td></tr></table>"
       this.panel!.webview.html = html
    }
 }
